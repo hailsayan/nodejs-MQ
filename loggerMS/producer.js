e16263423b0096e507039cc0b7f87c62807c8aa8
Psyon <sayan.h2020@gmail.com>
2024-03-09 15:19:37 +0330

    genesis

---
+++ loggerMS/producer.js
@@ -0,0 +1,11 @@
+const amqp = require('amqplib');
+class Producer {
+    
+}
+// connect to the rabbitmq server
+
+// create a new channel on that connection 1
+
+// create the exchange
+
+// publish the message to the exchange with a routing key
