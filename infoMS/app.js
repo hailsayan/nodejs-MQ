549f472202119736ceb8545b5988563122b7320b
Psyon <sayan.h2020@gmail.com>
2024-03-14 16:17:11 +0330

    add infoms

---
+++ infoMS/app.js
@@ -0,0 +1,6 @@
+// 1-Connect to the rabbitmq server
+// 2-Create a new channel
+// 3-Create the exchange
+// 4-Create the queue
+// 5-Bind the queue to the exchange
+// 6-Consume messages from the queue
