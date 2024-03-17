6f292581cbdd731d88ce08852fb4e8b9258dd5c3
Psyon <sayan.h2020@gmail.com>
2024-03-17 12:23:20 +0330

    add consumeMsg

--- infoMS/app.js
+++ infoMS/app.js
@@ -1,6 +1,25 @@
-// 1-Connect to the rabbitmq server
-// 2-Create a new channel
-// 3-Create the exchange
-// 4-Create the queue
-// 5-Bind the queue to the exchange
-// 6-Consume messages from the queue
+const amqp = require('amqplib');
+
+async function consumeMessages(){
+    // 1-Connect to the rabbitmq server
+    const connection = await amqp.connect('amqp://localhost');
+
+    // 2-Create a new channel
+    const channel = await connection.createChannel();
+
+    // 3-Create the exchange
+    await channel.assertExchange('logExchange', 'direct');
+
+    // 4-Create the queue
+    const q = await channel.assertQueue('InfoQueue');
+
+    // 5-Bind the queue to the exchange
+    await channel.bindQueue(q.queue, 'logExchange', 'Info');
+
+    // 6-Consume messages from the queue
+    channel.consume(q.queue, (msg)=>{
+        const data = JSON.parse(msg.content);
+        console.log(data);
+        channel.ack(msg);
+    });
+}
